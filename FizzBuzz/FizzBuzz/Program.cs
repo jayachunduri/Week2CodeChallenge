9531f991c0c162afa5f485d5adb6acd2c3e2af6d
jaya chunduri <jaya.chunduri@gmail.com>
2014-09-05 15:14:44 -0600

    Week2CodeChallenge

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        //Calling Fizz Buzz for numbers 0 to 20
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+                Fizzbuzz(i);
+        }
+
+        static void Fizzbuzz(int num)
+        {
+            if((num % 5 == 0) && (num % 3 == 0))
+                Console.WriteLine("Fizz Buzz");
+            else if(num % 5 == 0)
+                Console.WriteLine("Fizz");
+            else if(num % 3 == 0)
+                Console.WriteLine("Buzz");
+            else
+                Console.WriteLine(num);
+        }
+    }
+}
---
+++ LetterCounter/LetterCounter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterCounter
+{
+    class LetterCounter
+    {
+        static void Main(string[] args)
+        {
+            //calling CountLetters
+            CountLetters('i', "I love biscuits and gravy. It's the best breakfast ever");
+            CountLetters('n', "Never gonna give you up. Never gonna let you down");
+            CountLetters('s', "Sally sells seashells down by the seahore. She's from Sussex");
+        }
+
+        public static void CountLetters(char letter, string inString)
+        {
+            //variables to keep track of counts
+            int lower = 0, upper = 0;
+
+            //Changed the letter to lower and upper case
+            string lowercase = letter.ToString().ToLower();
+            string uppercase = letter.ToString().ToUpper();
+
+            //looping through each letter of inString and counting number of upper and lower case instances
+            for (int i = 0; i < inString.Length; i++)
+            {
+                if (inString[i].ToString() == lowercase)
+                    lower++;
+
+                else if (inString[i].ToString() == uppercase)
+                    upper++;
+            }
+
+            //Printing output
+            Console.WriteLine("Input: " + inString);
+            Console.WriteLine("Number of lower case: " + lower);
+            Console.WriteLine("Number of UPPER case: " + upper);
+            Console.WriteLine("Total number of " +letter +" s found: " +(upper+lower) +"\n");
+        }
+    }
+}
